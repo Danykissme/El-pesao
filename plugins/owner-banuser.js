e9552b8b8074bb22d222bc0709d65453f04e06cb
DanySR <ds0300835@gmail.com>
2024-07-05 00:52:35 -0400

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -5,10 +5,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = global.db.data.users[who]
-    if (!who) return m.reply(`🚩 Etiqueta a un usuario.`)
+    if (!who) return m.reply(`🛡️Etiqueta a un mortal.`)
     let users = global.db.data.users
     users[who].banned = true
-    conn.reply(m.chat, `🚩 @${who.split`@`[0]} ha sido baneado con exito, ya no podrá volver a usar mis comandos.`, m, { mentions: [who] })
+    conn.reply(m.chat, `🛡️ @${who.split`@`[0]} ha sido baneado con exito.`, m, { mentions: [who] })
 }
 handler.help = ['mban *@user*']
 handler.tags = ['owner']
