b94e73a4d8458a995358a0f66278a54fc8d71410
DanySR <ds0300835@gmail.com>
2024-07-05 03:56:28 -0400

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;おDanịel.xyz⁩;;\nFN:おDanịel.xyz⁩\nORG:おDanịel.xyz⁩\nTITLE:\nitem1.TEL;waid=5218261275256:5218261275256\nitem1.X-ABLabel:おDanịel.xyz⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:おDanịel.xyz⁩\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;DanySR;;\nFN:DanySR⁩\nORG:DanySR⁩\nTITLE:\nitem1.TEL;waid=5350619467:5350619467\nitem1.X-ABLabel:DanySR⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:DanySR⁩\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: 'DanySR⁩', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
 handler.tags = ['main']
