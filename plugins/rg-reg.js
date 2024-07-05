31cf7c98422bee9e762503472e2e7b86292dc28a
DanySR <ds0300835@gmail.com>
2024-07-05 05:02:17 -0400

    Update rg-reg.js

--- plugins/rg-reg.js
+++ plugins/rg-reg.js
@@ -7,26 +7,26 @@ let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  if (user.registered === true) return m.reply(`🚩 Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg* <Número de serie>`)
-  if (!Reg.test(text)) return m.reply(`🚩 Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.16*`)
+  if (user.registered === true) return m.reply(`🛡️ Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg* <Número de serie>`)
+  if (!Reg.test(text)) return m.reply(`🛡️ Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.16*`)
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) return m.reply('🚩 El nombre no puede estar vacío.')
-  if (!age) return m.reply('🚩 La edad no puede estar vacía.')
-  if (name.length >= 100) return m.reply('🚩 El nombre es demasiado largo.' )
+  if (!name) return m.reply('🛡️ El nombre no puede estar vacío.')
+  if (!age) return m.reply('🛡️ La edad no puede estar vacía.')
+  if (name.length >= 100) return m.reply('🛡️ El nombre es demasiado largo.' )
   age = parseInt(age)
-  if (age > 100) return m.reply('👴🏻 Wow el abuelo quiere jugar al bot.')
-  if (age < 5) return m.reply('🚼  hay un abuelo bebé jsjsj. ')
+  if (age > 60) return m.reply('👴🏻 Wow el abuelo quiere jugar al bot.')
+  if (age < 10) return m.reply('🚼  hay un abuelo bebé jsjsj. ')
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
   let img = await (await fetch(`https://tinyurl.com/ynvdqh3x`)).buffer()
-  let txt = ` –  *R E G I S T R O  -  U S E R*\n\n`
-      txt += `┌  ✩  *Nombre* : ${name}\n`
-      txt += `│  ✩  *Edad* : ${age} años\n`
-      txt += `│  ✩  *Numero de serie*\n`
-      txt += `└  ✩  ${sn}`
+  let txt = `❨ 🧙 ❩═☾⧐ᴘᴇʀғɪʟ - ᴜsᴇʀ⧏☽═❨ 🧛❩\n\n`
+      txt += `☾ ⧐ *Nombre* : ${name}\n`
+      txt += `☾ ⧐ *Edad* : ${age} años\n`
+      txt += `☾ ⧐ *Numero de serie*\n`
+      txt += `☾ ⧐ ${sn}`
 await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m)
 await m.react('✅')
 }
