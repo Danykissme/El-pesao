d53a99ed5706af385f62b08804db39a1153dd0dc
DanySR <ds0300835@gmail.com>
2024-07-05 12:38:45 -0400

    Update fun-love.js

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, command, text }) => {
 let love = `*❤️❤️ MEDIDOR DE AMOR ❤️❤️*
 *El amor de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
-*Deberias pedirle que sea tu  novia/o ?*
+*Deberias pedirle que sea tu  novi@😍 ?*
 `.trim()
 m.reply(love, null, { mentions: conn.parseMention(love) })}
 handler.help = ['love *@user*']
