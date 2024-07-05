0d43f7c088d5d4b04cd46fda94e971c086072ccf
DanySR <ds0300835@gmail.com>
2024-07-05 13:04:15 -0400

    Update fun-pregunta.js

--- plugins/fun-pregunta.js
+++ plugins/fun-pregunta.js
@@ -1,8 +1,8 @@
 let handler = async (m, { command, text }) => m.reply(`
-*⁉️ 𝐏𝐑𝐄𝐆𝐔𝐍𝐓𝐀𝐒 ⁉️*
+*⁉️ 𝑷𝑹𝑬𝑮𝑼𝑵𝑻𝑨𝑺 ⁉️*
   
 *𝙿𝚁𝙴𝙶𝚄𝙽𝚃𝙰:* ${text}
-*𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰:* ${['Si','Tal vez sí','Posiblemente','Probablemente no','No','Imposible'].getRandom()}
+*𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰:* ${['Awebo si','Tal vez sí','Posiblemente','Probablemente no','Obvio no','Imposible.jpg'].getRandom()}
 `.trim(), null, m.mentionedJid ? {
 mentions: m.mentionedJid
 } : {})
