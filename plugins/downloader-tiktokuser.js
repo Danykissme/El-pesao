2f85f8378dd7bf21da604a214ed6cbbdfe105bd3
DanySR <ds0300835@gmail.com>
2024-07-07 18:57:41 -0400

    Update downloader-tiktokuser.js

--- plugins/downloader-tiktokuser.js
+++ plugins/downloader-tiktokuser.js
@@ -1,8 +1,8 @@
 import Starlights from '@StarlightsTeam/Scraper'
 
 let handler = async (m, { conn, usedPrefix, command, text }) => {
-  if (!text) return conn.reply(m.chat, '🚩 Ingresa el nombre de usuario de TikTok que deseas buscar.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* yuuzu_u_`, m, rcanal)
-  await m.react('🕓')
+  if (!text) return conn.reply(m.chat, '🛡️ Ingresa el nombre de usuario de TikTok que deseas buscar.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* yuuzu_u_`, m, rcanal)
+  await m.react('⏳')
   try {
     let data = await Starlights.tiktokuser(text)
 
@@ -10,18 +10,18 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
       for (let i = 0; i < data.length; i++) {
         let video = data[i]
 
-        let txt = '`乂  T I K T O K  -  D O W N L O A D`\n\n'
-            txt += `    ✩  *Nro* : ${video.nro}\n`
-            txt += `    ✩  *Título* : ${video.title}\n`
-            txt += `    ✩  *Autor* : ${video.author}\n`
-            txt += `    ✩  *Duración* : ${video.duration} segundos\n`
-            txt += `    ✩  *Vistas* : ${video.views}\n`
-            txt += `    ✩  *Likes* : ${video.likes}\n`
-            txt += `    ✩  *Comentarios* : ${video.comments_count}\n`
-            txt += `    ✩  *Compartidos* : ${video.share_count}\n`
-            txt += `    ✩  *Publicado* : ${video.published}\n`
-            txt += `    ✩  *Descargas* : ${video.download_count}\n\n`
-            txt += `> 🚩 ${textbot}`
+        let txt = '`*᥀🎧୭ T I K T O K - D O W N L O A D ᥀🎧୭*`\n\n'
+            txt += `│☩  *Nro* : ${video.nro}\n`
+            txt += `│☩  *Título* : ${video.title}\n`
+            txt += `│☩  *Autor* : ${video.author}\n`
+            txt += `│☩  *Duración* : ${video.duration} segundos\n`
+            txt += `│☩  *Vistas* : ${video.views}\n`
+            txt += `│☩  *Likes* : ${video.likes}\n`
+            txt += `│☩  *Comentarios* : ${video.comments_count}\n`
+            txt += `│☩  *Compartidos* : ${video.share_count}\n`
+            txt += `│☩  *Publicado* : ${video.published}\n`
+            txt += `│☩  *Descargas* : ${video.download_count}\n\n`
+            txt += `> 🛡️ ${textbot}`
 
         await conn.sendFile(m.chat, video.dl_url, `video_${i + 1}.mp4`, txt, m, null, rcanal)
       }
