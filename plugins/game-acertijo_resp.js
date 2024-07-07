08057cc5f21dac62611e80fbf394ccedc0879d4d
DanySR <ds0300835@gmail.com>
2024-07-07 01:48:46 -0400

    Update game-acertijo_resp.js

--- plugins/game-acertijo_resp.js
+++ plugins/game-acertijo_resp.js
@@ -5,16 +5,16 @@ handler.before = async function (m) {
 let id = m.chat
 if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0
 this.tekateki = this.tekateki ? this.tekateki : {}
-if (!(id in this.tekateki)) return m.reply('Ese acertijo ya ha terminado!')
+if (!(id in this.tekateki)) return m.reply('🐌Ese acertijo ya ha terminado!🐌')
 if (m.quoted.id == this.tekateki[id][0].id) {
 let json = JSON.parse(JSON.stringify(this.tekateki[id][1]))
 if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
 global.db.data.users[m.sender].exp += this.tekateki[id][2]
-m.reply(`*Respuesta correcta!*\n+${this.tekateki[id][2]} Exp`)
+m.reply(`*╔🎉•°𝙵𝙴𝙻𝙸𝙲𝙸𝙳𝙰𝙳𝙴𝚂°•🎉╗*\n ```┃+${this.tekateki[id][2]} Exp \n┃ Buen trabajo 🎊``` \n╰┄꯭ׂ──𝖲𝖮𝖥𝖨𝖠-𝖡𝖮𝖳───┄ׅׅ┄꯭ׂ─╯`)
 clearTimeout(this.tekateki[id][3])
 delete this.tekateki[id]
-} else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`Casi lo logras!`)
-else m.reply('Respuesta incorrecta!')}
+} else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`🐌Casi lo logras!🐌`)
+else m.reply('❌Respuesta incorrecta!❌')}
 return !0
 }
 handler.exp = 0
