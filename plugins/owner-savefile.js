755eafe3e336bd503db7df7c2ea315a1572ce6c1
DanySR <ds0300835@gmail.com>
2024-07-07 03:44:43 -0400

    Update owner-savefile.js

--- plugins/owner-savefile.js
+++ plugins/owner-savefile.js
@@ -1,13 +1,13 @@
 import fs from 'fs'
 let handler = async (m, { text, usedPrefix, command }) => {
-if (!text) return m.reply(`🚩 Ingresa la Ruta y el nombre del Archivo junto al comando.`)
+if (!text) return m.reply(`🛡️ Ingresa la Ruta y el nombre del Archivo junto al comando.`)
 try {
-if (!m.quoted.text) return m.reply(`🚩 Responder al mensaje.`)
+if (!m.quoted.text) return m.reply(`🛡️ Responder al mensaje.`)
 let path = `${text}`
 await fs.writeFileSync(path, m.quoted.text)
-m.reply(`🚩 Guardado en *${path}*.`)
+m.reply(`🛡️ Guardado en *${path}*.`)
 } catch {
-await m.reply(`🚩 Responder al mensaje.`)
+await m.reply(`🛡️ Responder al mensaje.`)
 }}
 handler.command = ["savefile", "savejs", "savecmd"]
 handler.tags = ['owner']
