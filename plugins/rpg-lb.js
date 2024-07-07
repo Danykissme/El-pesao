05cb7e46c5edc0a098c7f0f40b666d4a9b7e0e32
DanySR <ds0300835@gmail.com>
2024-07-07 09:11:31 -0400

    Update rpg-lb.js

--- plugins/rpg-lb.js
+++ plugins/rpg-lb.js
@@ -9,26 +9,26 @@ let usersLim = sortedLim.map(enumGetKey)
 let usersLevel = sortedLevel.map(enumGetKey)
 let len = args[0] && args[0].length > 0 ? Math.min(5, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
 let text = `
-╭───═[ *Top ${len} Estrellas ⭐* ]═────⋆
+╭──֍☞ *Top ${len} Estrellas ⭐* ☜֍──⋆
 │╭───────────────···
-✩│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-✩│ ${sortedLim.slice(0, len).map(({ jid, limit }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} ⭐*`).join`\n✩│ `}
+│⭐Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
+│⭐ ${sortedLim.slice(0, len).map(({ jid, limit }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} ⭐*`).join`\n✩│ `}
 │╰────────────────···
-╰───────────═┅═──────────
+╰─────═SOFIA-BOT═───────
 
-╭───═[ *TOP ${len} XP 💫* ]═────⋆
+╭──֍☞ *TOP ${len} XP 💫* ☜֍──⋆
 │╭───────────────···
-✩│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-✩│ ${sortedExp.slice(0, len).map(({ jid, exp }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} 💫*`).join`\n✩│ `}
+│💫 Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
+│💫 ${sortedExp.slice(0, len).map(({ jid, exp }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} 💫*`).join`\n✩│ `}
 │╰────────────────···
-╰───────────═┅═──────────
+╰───────═SOFIA-BOT═───────
 
-╭───═[ *Top ${len} Nivel 📈* ]═────⋆
+╭──֍☞ *Top ${len} Nivel 📈* ☜֍───⋆
 │╭───────────────···
-✩│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-✩│ ${sortedLevel.slice(0, len).map(({ jid, level }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *Nivel ${level} 📈*`).join`\n✩│ `}
+│📈 Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
+│📈 ${sortedLevel.slice(0, len).map(({ jid, level }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *Nivel ${level} 📈*`).join`\n✩│ `}
 │╰────────────────···
-╰───────────═┅═──────────`.trim()
+╰───────═SOFIA-BOT═──────`.trim()
   m.reply(text, null, { mentions: conn.parseMention(text) })
 }
 handler.help = ['lb']
