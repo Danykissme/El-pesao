8120d14abb5da44c08e501e69f6f579bceb345ca
DanySR <ds0300835@gmail.com>
2024-07-10 04:03:57 -0400

    Update tools-ssweb.js

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -1,13 +1,13 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args }) => {
-if (!args[0]) return conn.reply(m.chat, '🚩 Ingresa el enlace de la página.', m, rcanal)
-await m.react('🕓')
+if (!args[0]) return conn.reply(m.chat, '🛡️ Ingresa el enlace de la página.', m, rcanal)
+await m.react('⌛')
 try {
 let ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer()
 conn.sendFile(m.chat, ss, 'error.png', listo, m, null, rcanal)
 await m.react('✅')
 } catch {
-await m.react('✖️')
+await m.react('❌')
 }}
 handler.help = ['ss', 'ssweb'].map(v => v + ' *<url>*')
 handler.tags = ['tools']
