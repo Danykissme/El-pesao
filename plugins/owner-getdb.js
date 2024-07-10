b4c2a3008b9e30dff5b7399edc3385cf4a7780d8
DanySR <ds0300835@gmail.com>
2024-07-10 02:53:36 -0400

    Update owner-getdb.js

--- plugins/owner-getdb.js
+++ plugins/owner-getdb.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 let handler = async (m, { conn, text }) => {
-    await m.react('🕓')
+    await m.react('⌛')
     let dl_url = await fs.readFileSync('./storage/databases/database.json')
     await conn.sendMessage(m.chat, { document: dl_url, mimetype: 'application/json', fileName: 'database.json' }, { quoted: m })
     await m.react('✅')
