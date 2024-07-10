031bb7982c07a513fe7211cd01291ba4d49f4e8b
DanySR <ds0300835@gmail.com>
2024-07-10 02:28:25 -0400

    Update img-ppcouple.js

--- plugins/img-ppcouple.js
+++ plugins/img-ppcouple.js
@@ -2,13 +2,13 @@ import Starlights from "@StarlightsTeam/Scraper"
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 try {
-await m.react('🕓')
+await m.react('⌛')
 let { women, man } = await Starlights.ppcouple("xd")
-await conn.sendFile(m.chat, women, 'thumbnail.jpg', `*» Chica*`, m, null, rcanal)
-await conn.sendFile(m.chat, man, 'thumbnail.jpg', `*» Chico*`, m, null, rcanal)
+await conn.sendFile(m.chat, women, 'thumbnail.jpg', `*» Chica👑*`, m, null, rcanal)
+await conn.sendFile(m.chat, man, 'thumbnail.jpg', `*» Chico🧢*`, m, null, rcanal)
 await m.react('✅')
 } catch {
-await m.react('✖️')
+await m.react('❌')
 }}
 handler.help = ['ppcouple']
 handler.tags = ['img']
