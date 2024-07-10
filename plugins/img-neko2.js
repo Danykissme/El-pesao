80ad4fef188ba2e3988c8a416499644ded22fb2b
DanySR <ds0300835@gmail.com>
2024-07-10 02:06:40 -0400

    Update img-neko2.js

--- plugins/img-neko2.js
+++ plugins/img-neko2.js
@@ -2,14 +2,14 @@ import fetch from 'node-fetch'
 
 let handler = async(m, { conn }) => {
 try {
-await m.react('🕓')
+await m.react('⌛')
 let response = await fetch('https://nekos.life/api/v2/img/neko')
 let data = await response.json()
 let imageUrl = data.url
 await conn.sendFile(m.chat, imageUrl, 'thumbnail.jpg', listo, m)
 await m.react('✅')
 } catch {
-await m.react('✖️')
+await m.react('❌')
 }}
 handler.command = ['neko2']
 handler.tags = ['img']
