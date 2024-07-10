d4c26191e73924a498126ad507c220f304feaee7
DanySR <ds0300835@gmail.com>
2024-07-10 02:35:17 -0400

    Update img-wallpaper.js

--- plugins/img-wallpaper.js
+++ plugins/img-wallpaper.js
@@ -1,14 +1,14 @@
 import { googleImage, pinterest } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `🚩 Ingresa un texto junto al comando.\n\n*Ejemplo:*\n*${usedPrefix + command}* Ai Hoshino`, m, rcanal)
-await m.react('🕓')
+if (!text) return conn.reply(m.chat, `🛡️ Ingresa un texto junto al comando.\n\n*Ejemplo:*\n*${usedPrefix + command}* Madara Uchiha`, m, rcanal)
+await m.react('⌛')
 try {
 let res = await (await googleImage('wallpaper' + text)).getRandom()
 await conn.sendFile(m.chat, res, 'thumbnail.jpg', `*» Wallpaper* : ${text ? text.capitalize() : false}`, m, null, rcanal)
 await m.react('✅')
 } catch {
-await m.react('✖️')
+await m.react('❌')
 }}
 handler.help = ['wallpaper *<búsqueda>*']
 handler.tags = ['img']
