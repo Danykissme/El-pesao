b32982e78ec7e4796cc67b07490db5dc0864388e
DanySR <ds0300835@gmail.com>
2024-07-10 04:09:15 -0400

    Update tools-togifaud.js

--- plugins/tools-togifaud.js
+++ plugins/tools-togifaud.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
-if (!m.quoted) return conn.reply(m.chat, `🚩 Responde a un *Video.*`, m, rcanal)
+if (!m.quoted) return conn.reply(m.chat, `🌻 Responde a un *Video.*`, m, rcanal)
 const q = m.quoted || m
 let mime = (q.msg || q).mimetype || ''
-if (!/(mp4)/.test(mime)) return conn.reply(m.chat, `🚩 Responde a un *Video.*`, m, rcanal)
-await m.react('🕓')
+if (!/(mp4)/.test(mime)) return conn.reply(m.chat, `🌻 Responde a un *Video.*`, m, rcanal)
+await m.react('⌛')
 let media = await q.download()
 conn.sendMessage(m.chat, { video: media, gifPlayback: true, caption: listo }, { quoted: m })
 await m.react('✅')
