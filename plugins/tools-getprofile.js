9b7aaf9cb288b37b778fa04c722319416c768da5
DanySR <ds0300835@gmail.com>
2024-07-10 03:51:26 -0400

    Update tools-getprofile.js

--- plugins/tools-getprofile.js
+++ plugins/tools-getprofile.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn, text }) => {
 let number = text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'
 let url = await conn.profilePictureUrl(number, 'image').catch(_ => './storage/img/avatar_contact.png')
-await m.react('🕓')
+await m.react('⌛')
 await conn.sendFile(m.chat, url, 'thumbnail.jpg', listo, m, null, rcanal)
 await m.react('✅')
 }
