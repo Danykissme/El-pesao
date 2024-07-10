4af8ca2ff731048ff1d8b0eb4589aabcc05b5228
DanySR <ds0300835@gmail.com>
2024-07-10 03:02:25 -0400

    Update owner-getsesion.js

--- plugins/owner-getsesion.js
+++ plugins/owner-getsesion.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 let handler = async (m, { conn, text }) => {
-    await m.react('🕓')
+    await m.react('⌛')
     let dl_url = await fs.readFileSync('./sessions/creds.json')
     await conn.sendMessage(m.chat, { document: dl_url, mimetype: 'application/json', fileName: 'creds.json' }, { quoted: m }).then(_ => m.react('✅'))
 }
