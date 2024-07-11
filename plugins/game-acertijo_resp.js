1a14a8bc7e7be18e7ad33e2c3cf128457c6a6e17
DanySR <ds0300835@gmail.com>
2024-07-11 07:07:59 -0400

    Update game-acertijo_resp.js

--- plugins/game-acertijo_resp.js
+++ plugins/game-acertijo_resp.js
@@ -10,13 +10,14 @@ if (m.quoted.id == this.tekateki[id][0].id) {
 let json = JSON.parse(JSON.stringify(this.tekateki[id][1]))
 if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
 global.db.data.users[m.sender].exp += this.tekateki[id][2]
-m.reply(`*╔🎉•°𝙵𝙴𝙻𝙸𝙲𝙸𝙳𝙰𝙳𝙴𝚂°•🎉╗*\n ┃+${this.tekateki[id][2]} Exp \n┃ Buen trabajo 🎊 \n╰┄꯭ׂ──𝖲𝖮𝖥𝖨𝖠-𝖡𝖮𝖳───┄ׅׅ┄꯭ׂ─╯`)
+m.reply(`*╔🎉•°𝙵𝙴𝙻𝙸𝙲𝙸𝙳𝙰𝙳𝙴𝚂°•🎉╗* \n┃+${this.tekateki[id][2]} Exp\n ┃ Buen trabajo🎊 \n╰┄꯭ׂ──𝖲𝖮𝖥𝖨𝖠-𝖡𝖮𝖳───┄ׅׅ┄꯭ׂ─╯`)
 clearTimeout (this.tekateki[id][3])
 delete this.tekateki[id]
 } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`🐌Casi lo logras!🐌`)
 else m.reply('❌Respuesta incorrecta!❌')}
 return !0
 }
+
 handler.exp = 0
 
 export default handler
