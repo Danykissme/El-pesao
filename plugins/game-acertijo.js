2b33d7a97507d2e86b4b676e3544d09b8184bbd9
DanySR <ds0300835@gmail.com>
2024-07-11 03:59:43 -0400

    Update game-acertijo.js

--- plugins/game-acertijo.js
+++ plugins/game-acertijo.js
@@ -13,23 +13,27 @@ return m.reply(`⏱️ ¡Espera ${msToTime(remainingTime)} antes de volver a usa
 conn.tekateki = conn.tekateki ? conn.tekateki : {}
 let id = m.chat
 if (id in conn.tekateki) {
-conn.reply(m.chat, '🛡️Todavía hay acertijos sin responder en este chat', conn.tekateki[id][0])
+conn.reply(m.chat, '🌻Todavía hay acertijos sin responder en este chat', conn.tekateki[id][0])
 return null
 }
 let tekateki = JSON.parse(fs.readFileSync(`./plugins/_acertijo.json`))
 let json = tekateki[Math.floor(Math.random() * tekateki.length)]
 let _clue = json.response
 let clue = _clue.replace(/[A-Za-z]/g, '_')
-let caption = `
-ⷮ *${json.question}*
-*• Tiempo:* ${(cooldown / 1000).toFixed(2)} segundos
-*• Bono:* +${poin} Exp
+let caption = '
+╭━ 💡ACERTIJO💡━
+┃*${json.question}*
+┃𝗧𝗜𝗘𝗠𝗣𝗢:* ${(cooldown / 1000).toFixed(2)} segundos
+┃ 𝗚𝗔𝗡𝗔𝗡𝗖𝗜𝗔:* +${poin} Exp
+┃ ♡ Buena Suerte
+┃ ʀᴇꜱᴘᴏɴᴅᴇ ᴀ ᴇꜱᴛᴇ ᴍᴇɴꜱᴀᴊᴇ 
+╰━━ ⬣ 🖤SOFIA-BOT 🌹⬣ ━━.
 `.trim()
 conn.tekateki[id] = [
 await conn.reply(m.chat, caption, m), json, poin,
 setTimeout(async () => {
 if (conn.tekateki[id]) {
-await conn.reply(m.chat, `Muy lento🐌!\n*Respuesta:* ${json.response}`, conn.tekateki[id][0])
+await conn.reply(m.chat, `╭━⏱️SIN TIEMPO!⏱️━\n┃ Respuesta:\n┃ * ${json.response}\n╰━⬣🖤SOFIA-BOT🌹⬣━ .`, conn.tekateki[id][0])
 delete conn.tekateki[id]
 }
 }, cooldown)
