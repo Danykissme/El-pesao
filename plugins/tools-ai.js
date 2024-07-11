1ae3f157ab0338e6fc59f5668c16f19b86fe7793
DanySR <ds0300835@gmail.com>
2024-07-11 04:22:32 -0400

    Add files via upload

---
+++ plugins/tools-ai.js
@@ -0,0 +1,15 @@
+import Starlights from '@StarlightsTeam/Scraper'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) return conn.reply(m.chat, `*Buenas tetas ,en que puedo ayudarte hoy :D`, m, rcanal)
+await m.react('💬')
+try {
+let { msg } = await Starlights.openAi(text)
+await conn.reply(m.chat, msg, m, rcanal)
+} catch {
+}}
+handler.help = ['sofia *<petición>*']
+handler.tags = ['tools']
+handler.command = /^(sofia|ai|ia|chatgpt|gpt)$/i
+handler.register = true
+export default handler
