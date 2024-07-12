049240d1efb5260211446c84b6bd66448653b36c
DanySR <ds0300835@gmail.com>
2024-07-11 23:26:59 -0400

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -4,11 +4,10 @@ let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetad
         mentions: users
     })
 }
-
 handler.help = ['invocar']
 handler.tags = ['group']
 handler.command = ['dinamica','todos']
+handler.reg = true
 handler.admin = true
 handler.group = true
-
 export default handler
