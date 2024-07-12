8ddc5cb85a017cf1c8a0cdd746d934bee10f87bf
DanySR <ds0300835@gmail.com>
2024-07-11 23:30:19 -0400

    Update group-invite.js

--- plugins/group-invite.js
+++ plugins/group-invite.js
@@ -3,17 +3,14 @@ if (!text) return conn.reply(m.chat, `🛡️ Ingrese el número al que quiere e
 if (text.includes('+')) return conn.reply(`🛡️ Ingrese el número todo junto sin el *+*`, m, rcanal)
 if (isNaN(text)) return conn.reply(m.chat, `*🛡️Ingrese sólo números más su código de país sin espacios*'
 let group = m.chat
-let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
- 
-      await conn.reply(text+'@s.whatsapp.net', `🛡️ *INVITACIÓN A GRUPO*\n\nUn usuario te invitó a unirte a este grupo \n\n${link}`, m, {mentions: [m.sender]})
+let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group) + await conn.reply(text+'@s.whatsapp.net', `🛡️ *INVITACIÓN A GRUPO*\n\nUn usuario te invitó a unirte a este grupo \n\n${link}`, m, {mentions: [m.sender]})
         m.reply(`✅Se envió un enlace de invitación al usuario.`) 
-
 }
 handler.help = ['invite *<535>*']
 handler.tags = ['group']
 handler.command = ['invite','invitar'] 
-handler.group = true
-handler.admin = false
+handler.group = false
+handler.admin = true
 handler.botAdmin = true
 
 export default handler
